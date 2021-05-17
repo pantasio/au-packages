313c792bd776d3aff5b2c16bc94d4948c0bef39d
Duc Hoang Le <hoang.lduc@gmail.com>
2021-05-17 14:10:53 +0700

    Update version 0.12.3

--- obsidian/tools/chocolateyInstall.ps1
+++ obsidian/tools/chocolateyInstall.ps1
@@ -5,8 +5,8 @@ $packageName = $env:chocolateyPackageName
 $packageArgs = @{
   packageName  = $packageName
   fileType     = 'exe'
-  url          = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v0.11.5/Obsidian.0.11.5.exe'
-  checksum     = '7cb53bfe50bfdaa66dad522f99a72c25da7984b7705c73a92781ff326afa3ffe'
+  url          = 'https://github.com/obsidianmd/obsidian-releases/releases/download/v0.12.3/Obsidian.0.12.3.exe'
+  checksum     = 'D5B92A5365AEA033E0C775AF890251C69B624E8D4BCEBD872B3A74F3218C195B'
   checksumType = 'sha256'
   silentArgs   = '/S'
 }
